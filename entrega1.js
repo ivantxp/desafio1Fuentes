9934179cfbbdc9e7e139cd75a95f3deff566fdcc
ivantxp <ivantxp@gmail.com>
2023-06-14 12:26:08 -0300

    14-06

---
+++ entrega1.js
@@ -0,0 +1,75 @@
+class ProductManager {
+    static ultimoId = 0;
+
+    constructor() {
+        this.products = [];
+    }
+
+    getProducts() {
+        console.log(this.products);
+    }
+    getpro;
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const repit = this.products.some((el) => el.code === code);
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("a dejado un campo de ingreso vacio en addProductos");
+        } else if (typeof price !== "number" || typeof stock !== "number") {
+            console.log(
+                `solo puede ingresar numeros en price y stock en su ingreso ${title}`
+            );
+        } else if (repit) {
+            console.log(
+                `el codigo "${code}" ya fue ingresado, ingrese uno nuevo`
+            );
+        } else {
+            ProductManager.ultimoId++;
+            this.products.push({
+                id: ProductManager.ultimoId,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+            });
+        }
+    }
+
+    getProductById(id) {
+        const targetID = this.products.find((el) => el.id === id);
+
+        if (targetID) {
+            console.log(`El producto buscado con id:${id} es: `, targetID);
+        } else {
+            console.error("not Found");
+            console.log(`el id:${id} no fue encontrado`);
+        }
+    }
+}
+
+//pruebas codigo
+const products = new ProductManager();
+
+products.getProducts();
+
+//codigos de prueba ingreso
+products.addProduct("Title 1", "Description 1", 40, "sin imag", "coded71", 15);
+
+//codigos de prueba ingreso
+products.addProduct("Title 2", "Description 2", 20, "sin imag", "coded1", 15);
+
+//codigos de prueba ingreso error numerico
+products.addProduct("Title 3", "Description 3", "24", "sin imag", "coded1", 15);
+
+//codigos de prueba ingreso error code
+products.addProduct("Title 3", "Description 3", 24, "sin imag", "coded71", 15);
+
+//codigos de prueba mostrar todo los productos
+products.getProducts();
+
+//codigos de prueba buscar por id
+products.getProductById(2);
+
+//codigos de prueba buscar por id
+products.getProductById(10);
